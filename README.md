0c0ce8a9515bf7b007155ed37ee06d6898350bf2
toledompm <45714268+toledompm@users.noreply.github.com>
2020-03-07 21:09:00 -0300

    Função para um objeto mover de acordo com outro objeto completa (#1)
    * creating aframe api
    
    * track de objeto relativo a outro objeto

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # montagem_de_peca
 Trabalho do 3° semestre com realidade virtual de Laboratório 3 com o Professor Giuliano
+## setup
+npm run build
---
+++ script.js
@@ -0,0 +1,52 @@
+const THREE = require('three');
+
+class AFrameObj{
+    constructor(params){
+        this.element = document.getElementById(params.id);
+        this.worldPosition = new THREE.Vector3();
+        if(params.movable){
+            this.movable = params.movable;
+        }
+    }
+
+    __setAttribute__(params){
+        this.element.setAttribute(params.attrName,params.attr);
+    }
+
+    updatePosition(){
+        this.element.object3D.getWorldPosition(this.worldPosition);
+    }
+
+    moveObject({newPos=this.worldPosition,dur="1500", easing="linear", elasticity="400"} = {}){
+        if(!this.movable) {
+            throw "Object not movable";
+        }
+        this.__setAttribute__({
+            attrName:"animation",
+            attr:"property: position; to:"+newPos.x+" "+newPos.y+" "+newPos.z+"; dur: "+dur+"easing: "+easing+"; elasticity: "+elasticity
+        });
+    }
+
+    trackReference(reference,distance,{interval="5"}={})
+    {
+        this.trackInterval = setInterval(() =>{
+            reference.updatePosition();
+            const referencePos = reference.worldPosition;
+            const multiplier =(referencePos.y-1.6<0)?-1:1;
+            const newX = referencePos.x * distance;
+            const newZ = referencePos.z *distance;
+            const newY = Math.sqrt(distance**2-newX**2-newZ**2) * multiplier;
+            const newPos={
+                x:newX,
+                y:newY+1.6,
+                z:newZ
+            }
+            this.moveObject({
+                newPos:newPos,
+                dur:"0",
+                elasticity:0
+            });
+        },interval);
+    }
+    
+}
