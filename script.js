9fa0c998867d2744ea1ef8bd5a67bee662014e22
toledompm <toledompm@gmail.com>
2020-06-14 22:20:43 -0300

    Creating onSelect function

--- script.js
+++ script.js
@@ -27,15 +27,16 @@ class AFrameObj{
         });
     }
 
-    trackReference(reference,distance,{interval="5"}={})
+    async trackReference(reference,distance,{interval=5}={})
     {
-        this.trackInterval = setInterval(() =>{
+        this.trackInterval = setInterval(() => {
             reference.updatePosition();
             const referencePos = reference.worldPosition;
             const multiplier =(referencePos.y-1.6<0)?-1:1;
             const newX = referencePos.x * distance;
-            const newZ = referencePos.z *distance;
+            const newZ = referencePos.z * distance;
             const newY = Math.sqrt(distance**2-newX**2-newZ**2) * multiplier;
+
             const newPos={
                 x:newX,
                 y:newY+1.6,
@@ -46,12 +47,39 @@ class AFrameObj{
                 dur:"0",
                 elasticity:0
             });
+            this.updatePosition();
         },interval);
+    }
+
+    onSelect(cursor, size, func, {ticks=0} = {}){
+        this.selectInterval = setInterval(() => {
+            if(ticks === 3){
+                func();
+                clearInterval(this.selectInterval);
+            }
+            this.updatePosition();
+            cursor.updatePosition();
+            const multiplier =(cursor.worldPosition.y-1.6<0)?-1:1;
+            const distance = Math.sqrt((this.worldPosition.x-cursor.worldPosition.x)**2 +
+                                       (this.worldPosition.z-cursor.worldPosition.z)**2);
+
+            const drawnPointX = cursor.worldPosition.x * distance;
+            const drawnPointZ = cursor.worldPosition.z * distance;
+            const drawnPointY = Math.sqrt(distance**2-drawnPointX**2-drawnPointZ**2) * multiplier + 1.6;
+
+            if(this.worldPosition.x <= drawnPointX+size && this.worldPosition.x >= drawnPointX-size &&
+            this.worldPosition.z <= drawnPointZ+size && this.worldPosition.z >= drawnPointZ-size &&
+            this.worldPosition.y <= drawnPointY+size && this.worldPosition.y >= drawnPointY-size){
+                ticks+=1;
+            }else ticks = 0;
+            console.log(ticks);
+        },250);
     }
 }
 
 const obj = new AFrameObj({id:"redCube", movable:true});
-
 const crosshair = new AFrameObj({id:"cursor"});
 
-obj.trackReference(crosshair, 3);
+obj.onSelect(crosshair,1,() =>{
+    obj.trackReference(crosshair,3);
+});
